625829ca344a45141db2a36f7be6a32b1dab9874
crowbarmaster <Freemanhl21.`>
2021-01-06 10:47:10 -0500

    We don't need to backup the debug symbol file when backing up with Advanced backup.

--- BedrockService/BedrockServerWrapper.cs
+++ BedrockService/BedrockServerWrapper.cs
@@ -391,7 +391,10 @@ namespace BedrockService
             foreach (DirectoryInfo dir in source.GetDirectories())
                 CopyFilesRecursively(dir, target.CreateSubdirectory(dir.Name));
             foreach (FileInfo file in source.GetFiles())
-                file.CopyTo(Path.Combine(target.FullName, file.Name));
+                if (file.Name != "bedrock_server.pdb")
+                {
+                    file.CopyTo(Path.Combine(target.FullName, file.Name));
+                }
             _log.Info("Finished Backup");
         }
 
